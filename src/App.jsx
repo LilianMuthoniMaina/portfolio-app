cdfa7ef835661df93ebe728ffd70a774e184c0eb
jareelireri-ops <jareel.ireri@student.moringaschool.com>
2026-02-16 15:47:07 +0300

    Jareel

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 <nav>
 <Link to="/">Home</Link> |
 <Link to="/about">About</Link> |
-<Link to="/projects">Projects</Link> |
+<Link to="/projects">My Projects</Link> |
 <Link to="/contact">Contact</Link> |
 <Link to="/profile">Profile</Link>
 </nav>
